70a3f81ce2636dff03da4130df21061f499dbf5a
LeJinge <jeremycarmona28@gmail.com>
2023-12-21 13:52:09 +0100

    Création des modèles

--- src/review/models.py
+++ src/review/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='tickets/', null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    in_independent_review = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def has_user_response(self, user):
+        return self.reviews.filter(user=user).exists()
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE, related_name='reviews', null=True, blank=True)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.TextField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    is_independent_review = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.headline
--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followed_by'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'followed_user'], name='unique_following')
+        ]
